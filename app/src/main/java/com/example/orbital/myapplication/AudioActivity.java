1a49f3290da3da094ea3d20f407f565070438f19
Ben Kazemi <ebaykazemi@googlemail.com>
2015-09-06 21:59:45 +0100

    added the licence preamble to the audio activity

--- app/src/main/java/com/example/orbital/myapplication/AudioActivity.java
+++ app/src/main/java/com/example/orbital/myapplication/AudioActivity.java
@@ -1,3 +1,24 @@
+/*
+   Haptic Feedback Case - Applications 
+   Copyright (C) 2015: Ben Kazemi, ben.kazemi@gmail.com
+   Copyright 2011-2013 Google Inc.
+   Copyright 2013 mike wakerly <opensource@hoho.com>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 package com.example.orbital.myapplication;
 
 import android.app.Activity;
